6f23c82923a2888baaff073f0158376cdce8d7f9
Sparta503 <chomunodisaa68@gmail.com>
2026-05-16 02:40:57 +0200

    Create AppNavigator.tsx

---
+++ Getit-React-Native/src/navigation/AppNavigator.tsx
@@ -0,0 +1,20 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import AuthNavigator from './AuthNavigator';
+import BottomTabs from './BottomTabs';
+import { RootStackParamList } from './types';
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export default function AppNavigator() {
+  return (
+    <Stack.Navigator
+      initialRouteName="Auth"
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen name="Auth" component={AuthNavigator} />
+      <Stack.Screen name="Main" component={BottomTabs} />
+    </Stack.Navigator>
+  );
+}
